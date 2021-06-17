2f756ce97833e85747ff0423e53052f5702c99cd
Anatoliy <swatqwer@gmail.com>
2021-06-17 18:38:31 +0700

    Создание конструктора и понятие инкапсуляция

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -6,10 +6,10 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point();
-            p1.x = 1;
+            Point p1 = new Point(1, 3, '*');
+            /*p1.x = 1;
             p1.y = 2;
-            p1.sym = '*';
+            p1.sym = '*';*/
             p1.Draw();
 
             Point p2 = new Point();
