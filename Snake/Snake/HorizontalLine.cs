b60b395658e4ec704b1f6f9dc0ce01b1c9754a29
Anatoliy <swatqwer@gmail.com>
2021-06-17 21:46:37 +0700

    Наследование

---
+++ Snake/Snake/HorizontalLine.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class HorizontalLine : Figure
+    {
+
+        public HorizontalLine(int xLeft, int xReight, int y, char sym)
+        {
+            pList = new List<Point>();
+            for (int x = xLeft; x <= xReight; x++)
+            {
+                Point p = new Point(x, y, sym);
+                pList.Add(p);
+            }
+            
+        }
+    }
+}
--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace Snake
 {
@@ -6,13 +10,23 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Point p1 = new Point(1, 3, '*');
-            p1.Draw();
+            Console.SetWindowSize(80, 25);
+            Console.SetBufferSize( 80, 25 );
 
-            Point p2 = new Point(4, 5, '#');
-            p2.Draw();
+            HorizontalLine upline = new HorizontalLine(0, 78, 0, '+');
+            HorizontalLine downline = new HorizontalLine(0, 78, 24, '+');
+            VerticalLine leftline = new VerticalLine(0, 24, 0, '+');
+            VerticalLine rightline = new VerticalLine(0, 24, 78, '+');
 
-          
+            upline.Draw();
+            downline.Draw();
+            leftline.Draw();
+            rightline.Draw();
+
+
+            Point p = new Point(4, 5, '*');
+            p.Draw();
+
             Console.ReadLine();
         }
         
